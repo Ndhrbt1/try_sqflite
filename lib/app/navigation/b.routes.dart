a68c6b0b80a65ad8b5be2a9d1b1b98cd3ce30af0
Ndhrbt1 <indahra04always@gmail.com>
2024-08-17 19:19:06 +0700

    build: pages

--- lib/app/navigation/b.routes.dart
+++ lib/app/navigation/b.routes.dart
@@ -6,5 +6,10 @@ class Routes {
   static const home = '/';
   static const sampleSatu = '/sample_satu';
   static const sampleDua = '/sample_dua';
+
+  static const productList = '/product_list';
+  static const productDetail = '/product_detail';
+  static const productAdd = '/product_add';
+  static const productEdit = '/product_edit';
   static const sampleTiga = '/sample_tiga';
 }
